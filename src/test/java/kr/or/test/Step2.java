717b37e2abff758a999d1eef93aa5f3690a85beb
User <User@DESKTOP-H15P611>
2021-05-28 17:24:15 +0900

    11111

--- src/test/java/kr/or/test/Step2.java
+++ src/test/java/kr/or/test/Step2.java
@@ -56,8 +56,15 @@ public class Step2 {
 		members[0] = memberVO;
 		members[1] = memberVO2;
 		members[2] = memberVO3;
+		//출력도 메서드를 바로 호줄하지 않고,외부 클래스에서 객체로 만들어서 메서드를 호출합니다.
 		MemberService memberService = new MemberService();
+		//외부클래스는 아래처럼 직접 접근해서 메서드나 변수를 사용할 수 없음
+		//외부클래스로 접근하려면,객체(실행가능한=메모리로딩)
+		//static 예약어는 컴파일시 메모리에 로딩이 되게 명시 
+		//매번 static으로 만들면 메모리 가득차서 프로실행이 느려지거나 멈춤
+		//객체로 만들면,호출(runtime)시 메모리에 로딩됩니다.->실행끝나면,반환
         memberService.printMember(members);
+        
 	}
     
 }
