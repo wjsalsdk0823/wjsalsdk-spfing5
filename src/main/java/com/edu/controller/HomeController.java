a9c53d963cc05f2f3deb9d077451b0630f400b8c
User <User@DESKTOP-H15P611>
2021-06-01 17:35:23 +0900

    6/1-1

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.edu.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 //외부 라이브러리(모듈) 사용 = import
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * 이 클래스는 MVC웹프로젝트를 최초로 생성시 자동으로 생성되는 클래스
@@ -18,8 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	//스프링빈(클래스)에서는 로거로 디버그를 합니다.= 로거객채
 	//로그중 slf4j(Spring Log For Java)
-	//private Logger logger = Logger.
-	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * 사용자요청(웹브라우저)을 받아서=@RdquestMapping 인터페이스를 사용해서 메서드명을 스프링이 구현
@@ -31,8 +33,9 @@ public class HomeController {
 	public String homepage(Model model) {//콜백메서드,자동실행.
 		String jspVar = "@서비스(DB)에서 치리한 결과";
 		model.addAttribute("jspObject", jspVar );
-		//home,jsp파일로 자료를 전송(스프링)하는 기능 model 인터페이스 객체(스프링이처리)에 내용만 채움
-		return "home";//확장자가 생략, jsp 생략	
+		logger.info("디버그 스프링로고사용: " + jspVar);//System.out 대신 logger객체를 사용
+		//home.jsp파일로 자료를 전송(스프링)하는 기능 model 인터페이스 객체(스프링이처리)에 내용만 채움
+		return "home/index";//확장자가 생략. jsp 생략	
 		}
 	
 }
